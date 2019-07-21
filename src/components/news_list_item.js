e0efb6103911952cf20ddaaeb810a3c0aa1426fc
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-21 21:58:17 +0530

    react_glamor

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {css} from 'glamor';
 
 const NewsItem = ({itemProp})=>{
+    let news_item_css = css({
+        padding: '20px',
+        boxSizing:'border-box',
+        borderBottom:'1px solid grey'
+    })
+    
+
     return(
-        <div>
+        <div className={`${news_item_css}`}>
             <h3>{itemProp.title}</h3>
             <div>{itemProp.feed}</div>
         </div>
